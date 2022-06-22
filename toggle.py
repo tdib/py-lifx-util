e8841d757f1465ad15fc16aa11b20d58d38a26ee
Thomas Dib <s3838765@student.rmit.edu.au>
2022-06-22 19:37:17 +1000

    Allow for devices file to be passed as command line argument

--- toggle.py
+++ toggle.py
@@ -3,8 +3,10 @@ import sys
 from lifxlan import Light
 
 # Load devices from json file
+# Uses ./devices.json if no second argument is specified
 try:
-  with open('./devices.json') as f:
+  device_data_file = sys.argv[2] if len(sys.argv) > 2 else './devices.json'
+  with open(device_data_file) as f:
     devices = json.load(f)
 except FileNotFoundError as e:
   print('The specified device data file could not be found.')
@@ -16,6 +18,8 @@ if not (device_data := devices.get(selected)):
   sys.stderr.write(f'The specified device ({selected}) could not be found.\n')
   sys.exit(1)
 device = Light(device_data['mac'], device_data['ip'])
+
+
 
 # Toggle light, attempting 5 times before exiting
 attempts = 0
